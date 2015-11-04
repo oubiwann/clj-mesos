964690ae717b42543010d56c76de70607dbc8f9e
Duncan McGreggor <oubiwann@gmail.com>
2015-11-04 10:50:11 -0600

    Updated version and deps.

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject clj-mesos "0.22.3-SNAPSHOT"
+(defproject clj-mesos "0.25.0-SNAPSHOT"
   :description "A fully-featured Mesos binding for Clojure"
   :url "http://github.com/dgrnbrg/clj-mesos"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [com.google.protobuf/protobuf-java "2.5.0"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [org.apache.mesos/mesos "0.22.1"]]
+                 [org.clojure/tools.logging "0.3.1"]
+                 [org.apache.mesos/mesos "0.25.0"]]
   :global-vars {*warn-on-reflection* true}
   :jvm-opts ["-XX:-OmitStackTraceInFastThrow" "-Xcheck:jni"]
   :deploy-repositories  [["releases" :clojars]]
